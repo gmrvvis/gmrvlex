53883c2c91362b48956b9ef4aa63f2b409beec0a
Sergio <sergio.e.galindo@gmail.com>
2015-04-23 18:18:52 +0200

    Created base for initial vocabulary implementation.

--- gmrvzeq/gmrvzeq.cpp
+++ gmrvzeq/gmrvzeq.cpp
@@ -1,3 +1,31 @@
 
 #include "gmrvzeq.h"
 
+namespace zeq
+{
+
+  namespace gmrv
+  {
+
+    ::zeq::Event serializeIDsColors( const data::IDColor& colors )
+    {
+      ::zeq::Event event( EVENT_IDCOLOR );
+      flatbuffers::FlatBufferBuilder& fbb = event.getFBB();
+      auto imageData = fbb.CreateVector( image.getDataPtr(),
+                                         image.getSizeInBytes( ));
+
+      ImageJPEGBuilder builder( fbb );
+      builder.add_data( imageData );
+
+      fbb.Finish( builder.Finish() );
+      return event;
+    }
+
+    data::IDColor* deserializeIDsColors( const ::zeq::Event& event )
+    {
+
+    }
+
+
+  }
+}
--- gmrvzeq/gmrvzeq.h
+++ gmrvzeq/gmrvzeq.h
@@ -1,4 +1,57 @@
+#ifndef GMRV_ZEQ_VOCABULARY_H
+#define GMRV_ZEQ_VOCABULARY_H
 
-#include <zeq/zeq.h>
+#include <zeq/types.h>
+#include <zeq/api.h>
 
+#include <gmrvzeq/idscolor_zeq_generated.h>
 
+namespace zeq
+{
+  namespace gmrv
+  {
+    namespace data
+    {
+      struct IDColor
+      {
+        IDColor(unsigned int id, std::vector<unsigned char> color)
+        : _id( id )
+        , _color( color.data() )
+        {}
+
+        unsigned int Id(){return _id;}
+        unsigned char* Color(){return _color;}
+
+        private:
+
+          unsigned int _id;
+          unsigned char* _color;
+
+      };
+
+      struct IDColorsArray
+      {
+
+      private:
+        unsigned int _size;
+        IDColor* _idcolors;
+      };
+
+    }
+    /**
+     * Serialize the given JPEG image into an Event of type EVENT_IMAGEJPEG.
+     * @param image the JPEG image.
+     * @return the serialized event.
+     */
+    ZEQ_API Event serializeIDsColors( const data::IDColor& colors );
+
+    /**
+     * Deserialize the given EVENT_IMAGEJPEG event into an JPEG image.
+     * @param event the zeq EVENT_IMAGEJPEG.
+     * @return the jpeg image.
+     */
+    ZEQ_API data::IDColor* deserializeIDsColors( const Event& event );
+
+  }
+}
+#endif
